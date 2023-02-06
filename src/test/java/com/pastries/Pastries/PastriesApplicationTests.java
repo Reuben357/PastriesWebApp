9fad6782caaca6124325fdf5a7e09aa3fb9c8656
Reuben <gathiireuben@gmail.com>
2023-02-06 09:14:02 +0300

    Added pastries application tests file

---
+++ src/test/java/com/pastries/Pastries/PastriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pastries.Pastries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
